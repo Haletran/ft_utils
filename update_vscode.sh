90cae457aaef978fd842dc6d6f516d39b7cd1628
Baptiste-Pasquier <bpasquier123@gmail.com>
2024-04-18 22:57:46 +0200

    Update update_vscode.sh

--- update_vscode.sh
+++ update_vscode.sh
@@ -1,12 +1,14 @@
 #!bin/sh
 
+current_dir=$(pwd)
+
 install_vscode()
 {
     wget --user-agent=Mozilla --content-disposition -E -c "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64"
     chmod 777 code-stable-x64-*.tar.gz
     tar -xzf code-stable-x64-*.tar.gz
     rm code-stable-x64-*.tar.gz
-    echo 'alias code="~/sgoinfre/VSCode-linux-x64/bin/code"' >> ~/.zshrc
+    echo 'alias code="$current_dir/VSCode-linux-x64/bin/code"' >> ~/.zshrc
 }
 
 main()
