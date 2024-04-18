4b8c6a69df6292b34bf1da920f4517279bc1b893
Haletran <bpasquier123@gmail.com>
2024-04-18 23:14:05 +0200

    Fix path alias and reload rc config

--- update_vscode.sh
+++ update_vscode.sh
@@ -1,24 +1,24 @@
 #!bin/sh
 
-current_dir=$(pwd)
-
 install_vscode()
 {
+    current_dir=$(pwd)
     wget --user-agent=Mozilla --content-disposition -E -c "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64"
     chmod 777 code-stable-x64-*.tar.gz
     tar -xzf code-stable-x64-*.tar.gz
     rm code-stable-x64-*.tar.gz
     if [ -n "$BASH_VERSION" ]; then
-        echo 'alias code="$current_dir/VSCode-linux-x64/bin/code"' >> ~/.bashrc
+        echo "alias code=\"${current_dir}/VSCode-linux-x64/bin/code\"" >> ~/.bashrc
+	source ~/.bashrc
     elif [ -n "$ZSH_VERSION" ]; then
-        echo 'alias code="$current_dir/VSCode-linux-x64/bin/code"' >> ~/.zshrc
+        echo "alias code=\"${current_dir}/VSCode-linux-x64/bin/code\"" >> ~/.zshrc
+	source ~/.zshrc
     fi
-
 }
 
 main()
 {
-    read -p "Do you want to install vscode? (y/n): " INSTALL_VSCODE
+    read -p "Do you want to update vscode? (y/n): " INSTALL_VSCODE
     if [ $? -eq 1 ] ; then echo -e "\nexit" && exit 1; fi
     if [ $INSTALL_VSCODE = "y" ]; then
         echo "Updating vscode..."
