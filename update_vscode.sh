84a1444dd1eb71291359618cd3b0190d9e6954cf
Haletran <bpasquier123@gmail.com>
2024-04-18 22:54:43 +0200

    add vscode updating script

---
+++ update_vscode.sh
@@ -0,0 +1,20 @@
+#!bin/sh
+
+install_vscode()
+{
+    wget --user-agent=Mozilla --content-disposition -E -c "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64"
+    chmod 777 code-stable-x64-*.tar.gz
+    tar -xzf code-stable-x64-*.tar.gz
+    rm code-stable-x64-*.tar.gz
+    echo 'alias code="~/sgoinfre/VSCode-linux-x64/bin/code"' >> ~/.zshrc
+}
+
+main()
+{
+    read -p "Do you want to install vscode? (y/n): " INSTALL_VSCODE
+    if [ $? -eq 1 ] ; then echo -e "\nexit" && exit 1; fi
+    if [ $INSTALL_VSCODE = "y" ]; then
+        install_vscode
+    fi
+}
+
