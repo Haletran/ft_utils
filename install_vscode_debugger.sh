5d5034e8083fdda69ef5558cce009c5ba4d8896f
Haletran <bpasquier123@gmail.com>
2024-07-01 12:54:08 +0200

    fix path

--- install_vscode_debugger.sh
+++ install_vscode_debugger.sh
@@ -9,22 +9,22 @@ install_vscode_debugger()
     fi
     if [ ! -f ".vscode/launch.json" ]; then
         echo "Creating launch.json file..."
-        wget -O vscode-debugger-utils/launch.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/launch.json
+        wget -O .vscode/launch.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/launch.json
     else
         echo "launch.json file already exists."
         echo "Backing up launch.json file..."
         mv .vscode/launch.json .vscode/launch.json.bkp
-        wget -O vscode-debugger-utils/launch.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/launch.json
+        wget -O .vscode/launch.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/launch.json
     fi
 
     if [ ! -f ".vscode/tasks.json" ]; then
         echo "Creating tasks.json file..."
-        wget -O vscode-debugger-utils/tasks.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/tasks.json
+        wget -O .vscode/tasks.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/tasks.json
     else
         echo "tasks.json file already exists."
         echo "Backing up tasks.json file..."
         mv .vscode/tasks.json .vscode/tasks.json.bkp
-        wget -O vscode-debugger-utils/tasks.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/tasks.json
+        wget -O .vscode/tasks.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/tasks.json
     fi
     echo "Debugger files updated."
 }
