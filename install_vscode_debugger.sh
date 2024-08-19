bce5ea4ab17b13ddebb78a5d8cb8fccd13fa87b4
Haletran <bpasquier123@gmail.com>
2024-08-19 19:35:49 +0200

    feat: add user input for project

--- install_vscode_debugger.sh
+++ install_vscode_debugger.sh
@@ -1,8 +1,17 @@
 #!bin/sh
 
+user_input()
+{
+    read -p "For which project do you want to install the debugger? " project_name
+    read -p "What args do you want to pass to the program? " args
+    echo "Project name: $project_name"
+    echo "Args: $args"
+}
+
 install_vscode_debugger()
 {
     current_dir=$(pwd)
+    user_input
     if [ ! -d ".vscode" ]; then
         echo "Creating .vscode directory..." 
         mkdir .vscode
@@ -10,11 +19,15 @@ install_vscode_debugger()
     if [ ! -f ".vscode/launch.json" ]; then
         echo "Creating launch.json file..."
         wget -O .vscode/launch.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/launch.json
+        sed -i "s/\"program\": \"\${workspaceFolder}\/a.out\"/\"program\": \"\${workspaceFolder}\/$project_name\"/g" .vscode/launch.json
+        sed -i "s/\"args\": \[\]/\"args\": \[\"$args\"\]/g" .vscode/launch.json
     else
         echo "launch.json file already exists."
         echo "Backing up launch.json file..."
         mv .vscode/launch.json .vscode/launch.json.bkp
         wget -O .vscode/launch.json https://raw.githubusercontent.com/Haletran/ft_utils/main/vscode-debugger-utils/launch.json
+        sed -i "s/\"program\": \"\${workspaceFolder}\/a.out\"/\"program\": \"\${workspaceFolder}\/$project_name\"/g" .vscode/launch.json
+        sed -i "s/\"args\": \[\]/\"args\": \[\"$args\"\]/g" .vscode/launch.json
     fi
 
     if [ ! -f ".vscode/tasks.json" ]; then
